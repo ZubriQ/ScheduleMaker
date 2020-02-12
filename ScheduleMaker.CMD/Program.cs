7e3ced981340eb86e10eadd4d68d32516608a44c
ZubriQ <zubrich157@gmail.com>
2020-02-12 21:13:25 +0700

    Проект создан.

---
+++ ScheduleMaker.CMD/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ScheduleMaker.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
