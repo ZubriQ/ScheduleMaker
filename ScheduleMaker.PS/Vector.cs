8081ad166f760b31921f9cd16d5b734af09516ad
ZubriQ <zubrich157@gmail.com>
2020-02-29 00:14:58 +0700

    Создан проект с Роем частиц.

---
+++ ScheduleMaker.PS/Vector.cs
@@ -0,0 +1,11 @@
+﻿namespace ScheduleMaker.PS
+{
+    public class Vector
+    {
+        /// <summary>Координата X.</summary>
+        public double CoordinateX { get; set; }
+
+        /// <summary>Координата Y.</summary>
+        public double CoordinateY { get; set; }
+    }
+}
